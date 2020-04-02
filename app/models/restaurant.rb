892190909901f0b27300f5728417873f1999ad70
Jack Bodley-Scott <jackso.bodleyscott@gmail.com>
2020-04-02 13:08:03 +0100

    pg-search added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,11 @@
 class Restaurant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_cuisine_name,
+    against: [ :cuisine, :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
